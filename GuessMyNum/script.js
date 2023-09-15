9a43dc0935fe2d100dfc55d500fc94b3dab35c85
Viraj Anand <iamtheviraj@gmail.com>
2023-09-15 14:38:56 +0530

    Added guessMyNum game logic

---
+++ GuessMyNum/script.js
@@ -0,0 +1,24 @@
+"use strict";
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "🎉 Correct Number !!!";
+// // console.log(document.querySelector(".message").textContent);
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// document.querySelector(".guess").value = 12;
+// console.log(document.querySelector(".guess").value);
+const secreatNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".number").textContent = secreatNumber;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ NO Number !!!";
+  } else if (guess === secreatNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number !!!";
+  } else if (guess < secreatNumber) {
+    document.querySelector(".message").textContent = "Your Guess is Lower";
+  } else if (guess > secreatNumber) {
+    document.querySelector(".message").textContent = "Your Guess is Higher";
+  }
+});
