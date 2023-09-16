af0567f5bf82e9be59fba1ebbfb9194c5539feb9
Viraj Anand <iamtheviraj@gmail.com>
2023-09-17 01:14:12 +0530

    compleated guessMyNum

--- GuessMyNum/script.js
+++ GuessMyNum/script.js
@@ -1,29 +1,52 @@
 "use strict";
-// console.log(document.querySelector(".message").textContent);
-// document.querySelector(".message").textContent = "🎉 Correct Number !!!";
-// // console.log(document.querySelector(".message").textContent);
-// document.querySelector(".number").textContent = 13;
-// document.querySelector(".score").textContent = 10;
-// document.querySelector(".guess").value = 12;
-// console.log(document.querySelector(".guess").value);
-const secreatNumber = Math.trunc(Math.random() * 20) + 1;
+let secreatNumber;
+let highScore = 0;
+function genNumber() {
+  secreatNumber = Math.trunc(Math.random() * 20) + 1;
+}
+genNumber();
 let score = Number(document.querySelector(".score").textContent);
 
-document.querySelector(".number").textContent = secreatNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
+  // NO NUMBER
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ NO Number !!!";
-  } else if (guess === secreatNumber) {
+  }
+  // CORRECT
+  else if (guess === secreatNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number !!!";
-  } else if (guess < secreatNumber) {
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30REM";
+    document.querySelector(".number").textContent = secreatNumber;
+  }
+  //LOW
+  else if (guess < secreatNumber) {
     document.querySelector(".message").textContent = "Your Guess is Lower";
     score--;
     document.querySelector(".score").textContent = score;
-  } else if (guess > secreatNumber) {
+  }
+  //HIGH
+  else if (guess > secreatNumber) {
     document.querySelector(".message").textContent = "Your Guess is Higher";
     score--;
     document.querySelector(".score").textContent = score;
   }
+});
+
+// to reset the game
+document.querySelector(".again").addEventListener("click", function () {
+  if (score >= highScore) {
+    highScore = score;
+    document.querySelector(".highscore").textContent = highScore;
+  }
+
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  genNumber();
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").style.width = "15REM";
+  document.querySelector("body").style.backgroundColor = "#222";
 });
