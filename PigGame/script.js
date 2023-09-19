317c546e704972ad78a6cfb9e9a869517781ed19
Viraj Anand <iamtheviraj@gmail.com>
2023-09-19 16:45:06 +0530

    Removed Bugs/Finished Game

--- PigGame/script.js
+++ PigGame/script.js
@@ -14,6 +14,7 @@ let rngDice;
 
 const score = [0, 0];
 let activePlayer = 0;
+let playing = true;
 
 score0El.textContent = '0';
 score1El.textContent = '0';
@@ -33,44 +34,48 @@ function playerWin() {
   document
     .querySelector(`.player--${activePlayer}`)
     .classList.add('player--winner');
-  roll.removeEventListener('click', function () {});
+  playing = false;
 }
 
 roll.addEventListener('click', function () {
-  diceEl.classList.remove('hidden');
-  rngDice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.src = `dice-${rngDice}.png`;
+  if (playing) {
+    diceEl.classList.remove('hidden');
+    rngDice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.src = `dice-${rngDice}.png`;
 
-  if (rngDice === 1) {
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // Switch Player
-    switchPlayer();
-  } else {
-    // add dice roll to current
-    currentScore += rngDice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    if (rngDice === 1) {
+      currentScore = 0;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // Switch Player
+      switchPlayer();
+    } else {
+      // add dice roll to current
+      currentScore += rngDice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
   }
 });
 
 hold.addEventListener('click', function () {
-  document.getElementById(`current--${activePlayer}`).textContent =
-    currentScore;
-  score[activePlayer] = score[activePlayer] + currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
-  currentScore = 0;
-  document.getElementById(`current--${activePlayer}`).textContent =
-    currentScore;
-  if (score[activePlayer] > 100) {
-    // Current Player Wins
-    console.log(`Player ${activePlayer + 1} Wins`);
-    playerWin();
-  } else {
-    // Switch Players and change active region
-    switchPlayer();
+  if (playing) {
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    score[activePlayer] = score[activePlayer] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    if (score[activePlayer] > 100) {
+      // Current Player Wins
+      console.log(`Player ${activePlayer + 1} Wins`);
+      playerWin();
+    } else {
+      // Switch Players and change active region
+      switchPlayer();
+    }
   }
 });
 
@@ -84,6 +89,10 @@ reset.addEventListener('click', function () {
   current1.textContent = currentScore;
   diceEl.classList.add('hidden');
   //Set player 1 as current
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
   activePlayer = 1;
-  switchPlayer;
+  switchPlayer();
+  playing = true;
 });
